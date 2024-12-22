8fa35c1e579e670e15273d6951633d47019d3b2d
Rauf Kutay Akyıldız <122848866+RaufkAk@users.noreply.github.com>
2024-12-22 16:32:22 +0300

    LoginPage first version

--- src/gui/LoginPage.java
+++ src/gui/LoginPage.java
@@ -2,14 +2,26 @@ package gui;
 
 import java.awt.EventQueue;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JPasswordField;
 
 public class LoginPage extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField textField;
+	private JLabel lblNewLabel_3;
+	private JPasswordField passwordField;
 
 	/**
 	 * Launch the application.
@@ -32,11 +44,58 @@ public class LoginPage extends JFrame {
 	 */
 	public LoginPage() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 580, 396);
+		setBounds(100, 100, 684, 341);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new EmptyBorder(1, 1, 1, 1));
 
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("WELCOME\n");
+		lblNewLabel.setBounds(19, 6, 88, 26);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Email");
+		lblNewLabel_1.setBounds(47, 50, 61, 16);
+		contentPane.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(47, 67, 130, 26);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("Password");
+		lblNewLabel_2.setBounds(47, 105, 61, 16);
+		contentPane.add(lblNewLabel_2);
+		
+		lblNewLabel_3 = new JLabel("New label");
+		lblNewLabel_3.setBackground(new Color(254, 255, 255));
+		lblNewLabel_3.setIcon(new ImageIcon(LoginPage.class.getResource("/resourcess/loginPageImage.png")));
+		lblNewLabel_3.setBounds(276, 0, 418, 319);
+		contentPane.add(lblNewLabel_3);
+		
+		JButton btnNewButton = new JButton("Login ");
+		btnNewButton.setBounds(76, 160, 117, 29);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Register");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(76, 192, 117, 29);
+		contentPane.add(btnNewButton_1);
+		
+		JLabel lblNewLabel_4 = new JLabel("Organize Your Kitchenware World!");
+		lblNewLabel_4.setFont(new Font("Lucida Grande", Font.PLAIN, 8));
+		lblNewLabel_4.setBounds(19, 32, 234, 16);
+		contentPane.add(lblNewLabel_4);
+		
+		passwordField = new JPasswordField();
+		passwordField.setBounds(47, 121, 130, 26);
+		contentPane.add(passwordField);
 
+		
+		
+	}
 }
